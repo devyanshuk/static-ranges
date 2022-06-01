76a9bfb025c32150c0d4118bc580b0e99855ac53
devyanshuk <devkoirala10@gmail.com>
2022-06-01 18:31:42 +0200

    move static_ranges to root dir

---
+++ static_ranges.hpp
@@ -0,0 +1,11 @@
+#ifndef __STATIC_RANGES_HPP__
+#define __STATIC_RANGES_HPP__
+
+#include "include/range_traits.hpp"
+#include "include/convenience.hpp"
+#include "include/concepts.hpp"
+#include "include/helpers.hpp"
+#include "include/views.hpp"
+#include "include/std.hpp"
+
+#endif //__STATIC_RANGES_HPP__
